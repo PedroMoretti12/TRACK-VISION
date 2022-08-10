7487fd61c063c7f34aec3a9ca1db0b9ec3d93e2a
vitor.macauba04 <vitor.macauba@sptech.school>
2022-08-10 14:29:49 -0300

    Atualização script e modelagem de BD

--- script-trackvison.sql
+++ script-trackvison.sql
@@ -1,13 +1,22 @@
 create database trackvision;
 use trackvision;
 
-create table usuario (
-idUsuario int primary key auto_increment,
-razaoSocial varchar(45),
-celular char(11),
-cpnj char(18),
-senha varchar (20)
+create table empresa (
+idEmpresa int primary key auto_increment,
+razaoSocial VARCHAR(45),
+cnpj char(18),
+email varchar(45)
 ) auto_increment = 1000;
+
+create table usuario (
+idUsuario int auto_increment,
+fkEmpresa int,
+foreign key(fkEmpresa) references empresa(idEmpresa), 
+primary key(idUsuario, fkEmpresa),
+nome varchar(45),
+email varchar(45),
+senha varchar (20)
+) auto_increment = 500;
 
 create table componentes (
 idComponente int primary key auto_increment,
@@ -16,30 +25,33 @@ descricao varchar(45)
 
 create table caixa (
 idCaixa int auto_increment,
-fkUsuario int,
-foreign key(fkUsuario) references usuario(idUsuario), 
-primary key(idCaixa, fkUsuario),
+fkEmpresa int,
+foreign key(fkEmpresa) references empresa(idEmpresa), 
+primary key(idCaixa, fkEmpresa),
 agencia char(4)
 ) auto_increment= 100;
 
 create table leitura (
 fkComponente int,
 fkCaixa int,
-fkUsuario int,
+fkEmpresa int,
 foreign key(fkComponente) references componentes(idComponente),
 foreign key(fkCaixa) references caixa(idCaixa),
-foreign key(fkUsuario) references usuario(idUsuario),
+foreign key(fkEmpresa) references empresa(idEmpresa),
 capTotal DECIMAL(5,2),
 capUso DECIMAL(5,2),
 ultimaLeitura DATETIME
 );
 
+desc empresa;
 desc usuario;
 desc componentes;
 desc caixa;
 desc leitura;
 
-insert into usuario values (null, 'Caixa Econômica Federal', '11922223333', '00.360.305/0001-04', 'caixa123');
+insert into empresa values (null, 'Caixa Econômica Federal', '00.360.305/0001-04', 'caixaeletronica@caixa.com');
+
+insert into usuario values (null, 1000, 'Pedro Henrique Ventura', 'pedroventura@caixa.com', 'pedro123_');
 
 insert into componentes values (null, 'Processador'),
 							   (null, 'Memória RAM'),
@@ -53,4 +65,4 @@ insert into caixa values (null, 1000, '1573'),
 insert into leitura values (2, 100, 1000, '16.00', '8.00', '2022-05-14 20:36:07'),
                            (3, 101, 1000, '447.00', '15.00', '2022-07-30 15:29:23');
                            
-select * from usuario, componentes, caixa, leitura where caixa.fkUsuario = idUsuario and leitura.fkUsuario = idUsuario and fkComponente = idComponente and fkCaixa = idCaixa;
+select * from empresa, usuario, componentes, caixa, leitura where usuario.fkEmpresa = idEmpresa and caixa.fkEmpresa = idEmpresa and leitura.fkEmpresa = idEmpresa and fkComponente = idComponente and fkCaixa = idCaixa;

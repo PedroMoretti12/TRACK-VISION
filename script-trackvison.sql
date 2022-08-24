ff79fc943d4957024508721f57f942cb5b7431e2
vitor.macauba04 <vitor.macauba@sptech.school>
2022-08-24 13:10:20 -0300

    Automatização em python

--- script-trackvison.sql
+++ script-trackvison.sql
@@ -26,23 +26,16 @@ agencia char(4)
 ) auto_increment= 100;
 
 create table leitura (
+idLeitura int primary key auto_increment,
 fkCaixa int,
 fkUsuario int,
 foreign key(fkCaixa) references caixa(idCaixa),
 foreign key(fkUsuario) references usuario(idUsuario),
-primary key(fkCaixa, fkUsuario),
-processador1 DECIMAL(3,1),
-processador2 DECIMAL(3,1),
-processador3 DECIMAL(3,1),
-processador4 DECIMAL(3,1),
-processador5 DECIMAL(3,1),
-processador6 DECIMAL(3,1),
-processador7 DECIMAL(3,1),
-processador8 DECIMAL(3,1),
-memoriaRAM DECIMAL(5,3),
-disco DECIMAL(6,3),
+processadorPorcentagem float,
+memoriaRAM float,
+disco float,
 ultimaLeitura DATETIME
-);
+)auto_increment = 2000;
 
 desc empresa;
 desc usuario;
@@ -52,11 +45,10 @@ desc leitura;
 insert into empresa values (null, 'Caixa Econômica Federal', '00.360.305/0001-04', 'caixaeletronica@caixa.com');
 
 insert into usuario values (null, 1000, 'Pedro Henrique Ventura', 'pedroventura@caixa.com', 'pedro123_');
-                               
+						SELECT * FROM usuario;
+                        
 insert into caixa values (null, 500, '1573'),
                          (null, 500, '0238');
-                         
-insert into leitura values (100, 500, '42.0', '39.1', '32.8', '34.4', '47.7', '43.8', '21.9', '31.2', '5.174', '24.618', '2022-05-14 20:36:07'),
-                           (101, 500, '41.0', '38.1', '31.8', '33.4', '46.7', '42.8', '20.9', '30.2', '4.235', '22.469', '2022-07-30 15:29:23');
-                           
+                                                    
 select * from empresa, usuario, caixa, leitura where usuario.fkEmpresa = idEmpresa and caixa.fkUsuario = idUsuario and leitura.fkUsuario = idUsuario and leitura.fkCaixa = idCaixa;
+select * from leitura;

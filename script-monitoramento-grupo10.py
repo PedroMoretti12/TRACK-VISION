832eb633ac8c23cb4c341905e559c68066ca34ff
vitor.macauba04 <vitor.macauba@sptech.school>
2022-08-25 14:46:24 -0300

    Atualização do script em python

--- script-monitoramento-grupo10.py
+++ script-monitoramento-grupo10.py
@@ -1,11 +1,10 @@
-#Grupo 10: TrackVision
-#Felipe Pires | 03221051
-#Isabela Hantke | 03221007
-#Rafaela Dias | 03221050
-#Verônica Zibord | 03221003
-#Vitor Macauba | 03221002
+# Grupo 10: TrackVision
+# Felipe Pires | 03221051
+# Isabela Hantke | 03221007
+# Rafaela Dias | 03221050
+# Verônica Zibord | 03221003
+# Vitor Macauba | 03221002
 
-#Verônica
 import psutil
 import time
 import mysql.connector
@@ -15,32 +14,31 @@ from datetime import date
 try:
 
     conn = mysql.connector.connect(
-    host= 'localhost', user= 'root', password= '#Gf44844181858', database= 'trackvision')
+        host='localhost', user='root', password='#Gf44844181858', database='trackvision')
     print("Conexão ao banco estabelecida!")
 
-    fk_caixa = int(input("Informe o caixa: "));
-    fk_usuario = int(input("Informe o usuário: "));
+    fkCaixa = int(input("Informe o código do caixa: "))
+    fkUsuario = int(input("Informe o seu código de usuário: "))
     cursor = conn.cursor()
-    inicio_segundos = 1
+    inicioSegundos = 1
 
-#Vitor
-    while True: 
-        ram_porcentagem = psutil.virtual_memory().percent
-        cpu_percent = psutil.cpu_percent(interval=1, percpu=False)
-        disco_usado = psutil.disk_usage("C:\\").percent
+    while True:
+        ramPorcentagem = psutil.virtual_memory().percent
+        cpuPercent = psutil.cpu_percent(interval=1, percpu=False)
+        discoUsado = psutil.disk_usage("C:\\").percent
         ultimaLeitura = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
         sql = "INSERT INTO leitura (fkCaixa, fkUsuario, processadorPorcentagem, memoriaRAM, disco, ultimaLeitura) VALUES (%s, %s, %s, %s, %s, (SELECT Now()))"
-        values = [fk_caixa, fk_usuario, cpu_percent, ram_porcentagem, disco_usado]
+        values = [fkCaixa, fkUsuario, cpuPercent,
+                  ramPorcentagem, discoUsado]
         cursor.execute(sql, values)
         conn.commit()
-        
-#Felipe
+
         print(ultimaLeitura)
-        print(inicio_segundos, "Captura de dados inserida!")
+        print(inicioSegundos, "Captura(s) de dados inserida(s).")
         print("\n")
-        inicio_segundos += 1
+        inicioSegundos += 1
         time.sleep(30)
-        
+
 except:
-    print("Houve um erro ao se conectar com o banco")
+    print("Houve um erro ao conectar-se ao banco.")

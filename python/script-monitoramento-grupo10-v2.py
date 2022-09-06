80d5805f3d4fdcde42db1bb7b73ccb8f87bb4f94
= <=>
2022-09-06 20:33:04 -0300

    Atualização de script do py - Simulação com mais de uma ATM

---
+++ python/script-monitoramento-grupo10-v2.py
@@ -0,0 +1,142 @@
+# Team 10: TrackVision -  Felipe Pires RA:03221051 | Isabela Hantke RA:03221007 | Rafaela Dias RA:03221050 | Verônica Zibord RA:03221003 | Vitor Macauba RA:03221002
+
+from dis import disco
+#from distutils.util import copydir_run_2to3
+import psutil
+import time
+import mysql.connector
+import datetime
+#from datetime import date
+import platform
+
+try:
+
+    conn = mysql.connector.connect(
+        host='localhost', user='Ella', password='urubu100', database='trackvision')
+    print("Conexão ao banco estabelecida!")
+except:
+    print("Houve um erro ao conectar-se ao banco.")
+
+#fkCaixa = int(input("Informe o código do caixa: "))
+fkUsuario = int(input("Informe o seu código de usuário: "))
+
+fkCaixa = 100
+fkCaixa2 = 101
+fkCaixa3 = 102 
+
+print("\n")
+cursor = conn.cursor()
+inicioSegundos = 0
+
+while True:
+
+    cpuPercent = psutil.cpu_percent(interval=1, percpu=False)
+    ramPorcentagem = psutil.virtual_memory().percent
+    discoUsado = psutil.disk_usage("C:\\").percent
+    ultimaLeitura = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+    cpu2 = (round(cpuPercent * 1.05, 1)) if (cpuPercent * 1.05 < 100) else 100
+    cpu3 = (round(cpuPercent * 1.15,1)) if (cpuPercent * 1.15 < 100) else 100
+
+    ramPorcentagem2 = (round(ramPorcentagem * 0.90, 1)) if (ramPorcentagem * 1.05 < 100) else 100
+    ramPorcentagem3 = (round(ramPorcentagem * 0.50, 1)) if (ramPorcentagem * 1.05 < 100) else 100
+
+    discoUsado2 = (round(discoUsado * 0.95,1)) if (cpuPercent * 1.05 < 100) else 100
+    discoUsado3 = (round(discoUsado * 0.33, 1)) if (cpuPercent * 1.05 < 100) else 100
+
+    maquinas = [
+        [fkCaixa, cpuPercent, ramPorcentagem, discoUsado],
+        [fkCaixa2, cpu2, ramPorcentagem2, discoUsado2],
+        [fkCaixa3, cpu3, ramPorcentagem3, discoUsado3]
+    ]
+
+    for computador in maquinas:
+        sql = "INSERT INTO leitura (fkCaixa, fkUsuario, processadorPorcentagem, memoriaRAM, disco, ultimaLeitura) VALUES (%s, %s, %s, %s, %s, (SELECT Now()))"
+        values = [computador[0], fkUsuario, computador[1], computador[2], computador[3]]
+        cursor.execute(sql, values)
+        conn.commit()
+        sopera = platform.system()
+
+        print("-"*30)
+        print(ultimaLeitura)
+        print("Sistema operacional utilizado: ",sopera)
+        inicioSegundos += 1
+        print(inicioSegundos, "Captura(s) de dados inserida(s).")
+
+        if computador[1] < 10:
+            print('CPU: Baixo uso.')
+        elif computador[1] < 70:
+            print('CPU: Uso médio (estável).')
+        else:
+            print('CPU: Alto uso, pode ser um risco!')
+
+        if  computador[2] < 10:
+            print('Memória RAM: Baixo uso, há bastante espaço livre!')
+        elif computador[2] < 60:
+            print('Memória RAM: Uso médio (estável).')
+        else:
+            print('Memória RAM: Alto uso, pode ser um risco!')
+
+        if  computador[3] < 10:
+            print('Disco: Baixo uso, há bastante espaço livre!')
+        elif computador[3] < 65:
+            print('Disco: Uso médio (estável).')
+        else:
+            print('Disco: Alto uso, pode ser um risco!')
+        
+        print("-"*30)
+        print("\n")
+        time.sleep(5.0)
+
+
+
+
+
+
+
+
+
+
+
+
+        # sql = "INSERT INTO leitura (fkCaixa, fkUsuario, processadorPorcentagem, memoriaRAM, disco, ultimaLeitura) VALUES (%s, %s, %s, %s, %s, (SELECT Now()))"
+        # values = [fkCaixa, fkUsuario, cpuPercent, ramPorcentagem, discoUsado]
+        # cursor.execute(sql, values)
+        # conn.commit()
+        # sopera = platform.system()
+
+        # print("-"*30)
+        # print(ultimaLeitura)
+        # print("Sistema operacional utilizado: ",sopera)
+        # inicioSegundos += 1
+        # print(inicioSegundos, "Captura(s) de dados inserida(s).")
+
+        # if cpuPercent >= 0 and cpuPercent <= 10:
+        #     print('CPU: Baixo uso.')
+        # elif cpuPercent >= 10 and cpuPercent <= 70:
+        #     print('CPU: Uso médio (estável).')
+        # elif cpuPercent >= 80 and cpuPercent <= 100:
+        #     print('CPU: Alto uso, pode ser um risco!')
+
+        # if ramPorcentagem >= 0 and ramPorcentagem <= 10:
+        #     print('Memória RAM: Baixo uso, há bastante espaço livre!')
+        # elif ramPorcentagem >= 10 and ramPorcentagem <= 60:
+        #     print('Memória RAM: Uso médio (estável).')
+        # elif ramPorcentagem >= 70 and ramPorcentagem <= 100:
+        #     print('Memória RAM: Alto uso, pode ser um risco!')
+
+        # if discoUsado >= 0 and discoUsado <= 10:
+        #     print('Disco: Baixo uso, há bastante espaço livre!')
+        #     print("-"*30)
+        #     print("\n")
+        # elif discoUsado >= 20 and discoUsado <= 65:
+        #     print('Disco: Uso médio (estável).')
+        #     print("-"*30)
+        #     print("\n")
+        # elif discoUsado >= 65 and discoUsado <= 100:
+        #     print('Disco: Alto uso, pode ser um risco!')
+        #     print("-"*30)
+        #     print("\n")     
+
+   
+

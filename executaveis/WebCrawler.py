ab4c08d9884fb5701597fdcf993287ab99268c41
LeandroVCosta <leandro.costa@sptech.school>
2022-10-14 12:59:12 -0300

    Adicionando Executaveis Jar e Python

---
+++ executaveis/WebCrawler.py
@@ -0,0 +1,69 @@
+import requests
+import pyodbc
+from bs4 import BeautifulSoup
+
+
+#Conexao
+
+server = 'trackvisiondb.database.windows.net'
+database = 'trackvisiondb'
+username = 'CloudSA49c766d4'
+password = 'Urubu1004'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+conn = pyodbc.connect('DRIVER='+driver+';'
+                      'SERVER=tcp:'+server+';'
+                      'PORT=1433;'
+                      'DATABASE='+database+';'
+                       'UID='+username+';'
+                                                                                                 'PWD='+ password)
+cursor = conn.cursor()
+cursor.execute("select * from Usuario;")
+row = cursor.fetchone()
+while row:
+    print (row)
+    row = cursor.fetchone()
+
+
+#Escolhe a Ação do Banco
+id = ''
+escolha = input('Escolha um Banco: \n'
+                '1-Banco do Brasil \n'
+                '2-Banco Pan \n'
+                '3-Bradesco')
+if(escolha == '1'):
+    id = 'pair_18604'
+elif(escolha == '2'):
+    id = 'pair_18614'
+elif(escolha == '3'):
+    id = 'pair_18605'
+else:
+    print('Nenhuma Opção Válida Encerrando o Web Crawler')
+
+if(id != ''):
+ page = requests.get('https://br.investing.com/equities/')
+ soup = BeautifulSoup(page.content, 'html.parser')
+
+#Procura os Dados
+ tagbanco = soup.find('tr',{'id':id}).contents[1].findNext('a')
+ tagultima = soup.find('tr',{'id':id}).contents[1].findNext('td').contents[0]
+ tagmaxima = tagultima.findNext('td')
+ tagminima = tagmaxima.findNext('td')
+ tagvariacao = tagminima.findNext('td')
+
+ banco = tagbanco.contents[0]
+ ultima = float(tagultima.replace(',','.'))
+ minima = float(tagminima.contents[0].replace(',','.'))
+ maxima = float(tagmaxima.contents[0].replace(',','.'))
+ variacao = float(tagvariacao.contents[0].replace(',','.'))
+
+ print(banco)
+ print(ultima)
+ print(minima)
+ print(maxima)
+ print(variacao)
+
+syntax = "update cotacao set atual = ?, minimo = ?, maximo = ?, variacao = ? where fkBanco = 1"
+values = [ultima,minima,maxima,variacao]
+cursor.execute(syntax,values)
+cursor.commit()

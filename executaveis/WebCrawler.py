9d9d6b159a68707bb3ea110ffb8343381c7362be
LeandroVCosta <leandrocosta@sptech.school>
2022-10-19 22:37:49 -0300

    Pequenas Alterações

--- executaveis/WebCrawler.py
+++ executaveis/WebCrawler.py
@@ -11,7 +11,7 @@ server = 'trackvisiondb.database.windows.net'
 database = 'trackvisiondb'
 username = 'CloudSA49c766d4'
 password = 'Urubu1004'
-driver= '{ODBC Driver 17 for SQL Server}'
+driver = '{ODBC Driver 17 for SQL Server}'
 
 conn = pyodbc.connect('DRIVER='+driver+';'
                       'SERVER=tcp:'+server+';'
@@ -23,60 +23,68 @@ cursor = conn.cursor()
 
 
 #Escolhe a Ação do Banco
-id = ''
 fkBanco = 0
 escolha = input('Escolha um Banco: \n'
                 '1-Banco do Brasil \n'
                 '2-Bradesco \n'
                 '3-Banco Pan\n')
 if(escolha == '1'):
-    id = 'pair_18604'
+    pagehtml = 'https://www.infomoney.com.br/cotacoes/b3/acao/banco-do-brasil-bbas3/'
     fkBanco = 1
 elif(escolha == '2'):
-    id = 'pair_18605'
+    pagehtml = 'https://www.infomoney.com.br/cotacoes/b3/acao/bradesco-bbdc3/'
     fkBanco = 2
 elif(escolha == '3'):
-    id = 'pair_18614'
+    pagehtml = 'https://www.infomoney.com.br/cotacoes/b3/acao/banco-pan-bpan4/'
     fkBanco = 3
 else:
     print('Nenhuma Opção Válida Encerrando o Web Crawler')
 
 if(id != ''):
- page = requests.get('https://br.investing.com/equities/')
+ page = requests.get(pagehtml)
  soup = BeautifulSoup(page.content, 'html.parser')
 
 #Procura os Dados
- tagbanco = soup.find('tr',{'id':id}).contents[1].findNext('a')
- tagultima = soup.find('tr',{'id':id}).contents[1].findNext('td').contents[0]
- tagmaxima = tagultima.findNext('td')
- tagminima = tagmaxima.findNext('td')
- tagvariacao = tagminima.findNext('td')
 
- banco = tagbanco.contents[0]
+ corpodado = soup.find('div', {'class':'line-info'})
+ tagultima = corpodado.findNext('div').findNext('p').contents[0]
+ tagvariacao = tagultima.findNext('div').findNext('p').contents[0]
+ tagminima = tagvariacao.findNext('div').findNext('p').contents[0]
+ tagmaxima = tagminima.findNext('div').findNext('p').contents[0]
+
+#Transforma em Números
  ultima = float(tagultima.replace(',','.'))
- minima = float(tagminima.contents[0].replace(',','.'))
- maxima = float(tagmaxima.contents[0].replace(',','.'))
- variacao = float(tagvariacao.contents[0].replace(',','.'))
+ variacao = float(tagvariacao.replace('%',''))
+ minima = float(tagminima.replace(',','.'))
+ maxima = float(tagmaxima.replace(',','.'))
 
- print(banco)
+ print(ultima)
+ print(variacao)
+ print(minima)
+ print(maxima)
+ print('oie')
 
  #INSERT NO BANCO
 
- syntax = "update cotacao set atual = ?, minimo = ?, maximo = ?, variacao = ? where fkBanco = (select id from Banco where nomeBanco = ?)"
- values = [ultima,minima,maxima,variacao,banco]
+ syntax = "update cotacao set atual = ?, minimo = ?, maximo = ?, variacao = ? where fkBanco = ?"
+ values = [ultima,minima,maxima,variacao,fkBanco]
  cursor.execute(syntax,values)
  cursor.commit()
  count = cursor.rowcount
  #CASO NÃO ESTEJA NA TABELA COTAÇÃO
  if(count == 0):
-    syntax = "insert into cotacao values ((select id from Banco where nomeBanco = ?),0,0,0,0)"
-    values = [banco]
+    syntax = "insert into cotacao values ?,?,?,?,?)"
+    values = [fkBanco,ultima, minima, maxima, variacao]
     cursor.execute(syntax, values)
     cursor.commit()
-    syntax = "update cotacao set atual = ?, minimo = ?, maximo = ?, variacao = ? where fkBanco = (select id from Banco where nomeBanco = ?)"
-    values = [ultima, minima, maxima, variacao, banco]
-    cursor.execute(syntax, values)
-    cursor.commit()
+
+syntax = "select nomeBanco from Banco where id = ?"
+values = (fkBanco)
+cursor.execute(syntax, values)
+row = cursor.fetchone
+for row in cursor:
+ print(row)
+ banco = row.nomeBanco
 
 labels = ["Ultima","Minima","Maxima","Variação"]
 data = [ultima,minima,maxima,variacao]
@@ -87,9 +95,3 @@ ax.set_ylabel("Valor em R$")
 ax.set_xlabel("Contexto")
 plt.show()
 
-syntax = "select * from cotacao where fkBanco = (select id from banco where nomeBanco = ?)"
-values = (banco)
-cursor.execute(syntax,values)
-row = cursor.fetchone
-for row in cursor:
-    print(row)

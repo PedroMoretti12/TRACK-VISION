85655bba97e84aa7b0e9943c93fecd070eae8fac
MichaMendes <michelly.silva@sptech.school>
2022-12-06 21:38:19 -0300

    individual

--- site/src/controllers/dataController.js
+++ site/src/controllers/dataController.js
@@ -156,22 +156,6 @@ class dataController {
 
     }
 
-    static obterMedia(req, res) {
-
-        var idAgencia = req.params.idAgencia;
-        console.log(idAgencia)
-
-        dataModel.obterMedia(idAgencia)
-        .then(response => {
-            res.status(200).json(response)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).send()
-        })
-
-    }
-
     static obterRegressao(req, res) {
 
         var idAgencia = req.params.idAgencia;
